0109a688a51b734b41d944dcbba1014e9d0b5c4d
JACOBDUCAS <jducas182@gmail.com>
2024-09-30 15:08:59 -0400

    base minimax

--- Projet_Divercite_A2024/Divercite/my_player.py
+++ Projet_Divercite_A2024/Divercite/my_player.py
@@ -15,13 +15,14 @@ class MyPlayer(PlayerDivercite):
     def __init__(self, piece_type: str, name: str = "MyPlayer"):
         """
         Initialize the PlayerDivercite instance.
-
         Args:
             piece_type (str): Type of the player's game piece
             name (str, optional): Name of the player (default is "bob")
             time_limit (float, optional): the time limit in (s)
         """
         super().__init__(piece_type, name)
+        self.seen_boards = []
+        self.max_depth = 3
 
     def compute_action(self, current_state: GameState, remaining_time: int = 1e9, **kwargs) -> Action:
         """
@@ -34,5 +35,47 @@ class MyPlayer(PlayerDivercite):
             Action: The best action as determined by minimax.
         """
 
-        #TODO
-        raise MethodNotImplementedError()
+        _, best_action = self.max_value(current_state, float('-inf'), float('inf'), 0)
+        return best_action
+
+
+    def max_value(self, state: GameState, alpha: int, beta: int, depth: int):
+        # if terminal return
+        if depth >= self.max_depth:
+            return state.scores[self.get_id()], None
+        
+        score = float('-inf')
+        action = None
+        # generate_possible_heavy_actions sera changée par fonction prenant en considération les heuristiques
+        for a in state.generate_possible_heavy_actions():
+            next_state = a.get_next_game_state()
+            value, _ = self.min_value(next_state, alpha, beta, depth + 1)
+            if value > score:
+                score = value
+                action = a
+                alpha = max(alpha, score)
+
+            if score >= beta:
+                return score, action
+            
+        return score, action
+
+    def min_value(self, state: GameState, alpha: int, beta: int, depth: int):
+        if depth >= self.max_depth:
+            return state.scores[self.get_id()], None
+        
+        score = float('inf')
+        action = None
+
+        for a in state.generate_possible_heavy_actions():
+            next_state = a.get_next_game_state()
+            value, _ = self.max_value(next_state, alpha, beta, depth + 1)
+            if value < score:
+                score = value
+                action = a
+                beta = min(beta, score)
+
+            if score <= alpha:
+                return score, action
+            
+        return score, action
