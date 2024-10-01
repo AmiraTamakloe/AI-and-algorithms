b0032f6bed09475ddb9e14a287de52cf42f0d7b0
JACOBDUCAS <jducas182@gmail.com>
2024-10-01 15:28:21 -0400

    correctly check if board already seen

--- Projet_Divercite_A2024/Divercite/my_player.py
+++ Projet_Divercite_A2024/Divercite/my_player.py
@@ -4,7 +4,7 @@ from seahorse.game.game_state import GameState
 from game_state_divercite import GameStateDivercite
 from seahorse.utils.custom_exceptions import MethodNotImplementedError
 from board_divercite import BoardDivercite
-import copy
+import random
 class MyPlayer(PlayerDivercite):
     """
     Player class for Divercite game that makes random moves.
@@ -22,8 +22,9 @@ class MyPlayer(PlayerDivercite):
             time_limit (float, optional): the time limit in (s)
         """
         super().__init__(piece_type, name)
-        self.seen_boards = []
-        self.max_depth = 3
+        self._seen_transposition_tables = {}
+        self.max_depth = 5
+        self.node_count = 0
 
     def compute_action(self, current_state: GameState, remaining_time: int = 1e9, **kwargs) -> Action:
         """
@@ -36,67 +37,78 @@ class MyPlayer(PlayerDivercite):
             Action: The best action as determined by minimax.
         """
         _, best_action = self.max_value(current_state, float('-inf'), float('inf'), 0)
+        print("Node count: ", self.node_count)
         return best_action
 
 
     def max_value(self, state: GameState, alpha: int, beta: int, depth: int):
-        # if terminal return
-        if depth >= self.max_depth:
-            return state.scores[self.get_id()], None
+        self.node_count += 1
+        if state in self._seen_transposition_tables:
+            if self._seen_transposition_tables[state]['depth'] >= depth:
+                return self._seen_transposition_tables[state]['score'], self._seen_transposition_tables[state]['action']
+                    
+
+        if state.is_done() or depth >= self.max_depth:
+            player_id, other_player_id = self.get_player_ids(state)
+            return state.scores[player_id] - state.scores[other_player_id], None
+
         
         score = float('-inf')
         action = None
-        # generate_possible_heavy_actions sera changée par fonction prenant en considération les heuristiques
         for a in state.generate_possible_heavy_actions():
             next_state = a.get_next_game_state()
-            if self.check_already_seen(next_state):
-                continue
-
             value, _ = self.min_value(next_state, alpha, beta, depth + 1)
+                
             if value > score:
                 score = value
                 action = a
                 alpha = max(alpha, score)
 
             if score >= beta:
+                self.record_seen_board(state, score, action, depth)
                 return score, action
-            
+    
+        self.record_seen_board(state, score, action, depth)
         return score, action
 
     def min_value(self, state: GameState, alpha: int, beta: int, depth: int):
-        if depth >= self.max_depth:
-            return state.scores[self.get_id()], None
+        self.node_count += 1
+        if state in self._seen_transposition_tables:
+            if self._seen_transposition_tables[state]['depth'] >= depth:
+                    return self._seen_transposition_tables[state]['score'], self._seen_transposition_tables[state]['action']
+
+        if state.is_done() or depth >= self.max_depth:
+            player_id, other_player_id = self.get_player_ids(state)
+            return state.scores[player_id] - state.scores[other_player_id], None
         
         score = float('inf')
         action = None
 
         for a in state.generate_possible_heavy_actions():
             next_state = a.get_next_game_state()
+            
             value, _ = self.max_value(next_state, alpha, beta, depth + 1)
+
             if value < score:
                 score = value
                 action = a
                 beta = min(beta, score)
 
             if score <= alpha:
+                self.record_seen_board(state, score, action, depth)
                 return score, action
             
+        
+        self.record_seen_board(state, score, action, depth)
         return score, action
-    
-    def check_already_seen(self, state: GameState):
-        board = BoardDivercite(state.get_rep().get_env(), state.get_rep().get_dimensions()).get_grid()
-        for i in range(4):
-            if board in self.seen_boards:
-                return True
-            board = self.rotate_matrix_90(board)
-        self.seen_boards.append(copy.deepcopy(board))
-        return False
-    
-    def rotate_matrix_90(self, matrix):
-        n = len(matrix)
-        for i in range(n):
-            for j in range(i, n):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        for i in range(n):
-            matrix[i].reverse()
-        return matrix
+
+
+    def record_seen_board(self, hash, score, action, depth):
+        self._seen_transposition_tables[hash] = {}
+        self._seen_transposition_tables[hash]['score'] = score
+        self._seen_transposition_tables[hash]['action'] = action
+        self._seen_transposition_tables[hash]['depth'] = depth
+
+    def get_player_ids(self, state):
+        keys = list(state.scores.keys())
+        return (keys[0], keys[1]) if keys[0] == self.get_id() else (keys[1], keys[0])
