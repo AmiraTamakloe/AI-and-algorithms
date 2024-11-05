d4f13089597f2d05ae4d324c0be07c5a1fa78091
NourZahreddine <nour.zahreddine@polymtl.ca>
2024-11-05 16:42:53 -0500

    Partie 1

--- Devoir2_INF8175_A24/RechercheLocale/code/solver_advanced.py
+++ Devoir2_INF8175_A24/RechercheLocale/code/solver_advanced.py
@@ -6,6 +6,32 @@ def solve(schedule):
     :param schedule: object describing the input
     :return: a list of tuples of the form (c,t) where c is a course and t a time slot. 
     """
-    # Add here your agent
 
-    raise Exception("Agent is not implemented")
+    solution = dict()
+    courses_dict = dict()
+
+    for course1, course2 in schedule.conflict_list:
+        if course1 not in courses_dict:
+            courses_dict[course1] = []
+        if course2 not in courses_dict:
+            courses_dict[course2] = []
+        
+        courses_dict[course1].append(course2)
+        courses_dict[course2].append(course1)
+    
+    sorted_courses = sorted(courses_dict.keys(), key=lambda c: len(courses_dict[c]), reverse=True)
+
+    for course in sorted_courses:  
+        time_slots_taken = set()
+        for neighbors in courses_dict[course]:
+            if neighbors in solution:
+                time_slots_taken.add(solution[neighbors])
+        
+        time_slot = 1
+        while time_slot in time_slots_taken:
+            time_slot += 1
+        solution[course] = time_slot
+    
+    return solution
+
+    # raise Exception("Agent is not implemented")
