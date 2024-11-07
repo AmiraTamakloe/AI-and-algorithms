0bb0726bdbda78d8b5ded41d21b9014b7a1f9953
AmiraTamakloe <89823163+AmiraTamakloe@users.noreply.github.com>
2024-11-06 20:10:13 -0500

    time slot schedule algorithm implementation

--- Devoir2_INF8175_A24/RechercheLocale/code/solver_advanced.py
+++ Devoir2_INF8175_A24/RechercheLocale/code/solver_advanced.py
@@ -6,6 +6,26 @@ def solve(schedule):
     :param schedule: object describing the input
     :return: a list of tuples of the form (c,t) where c is a course and t a time slot. 
     """
-    # Add here your agent
 
-    raise Exception("Agent is not implemented")
+    solution = {} 
+    courses = {}
+
+    for c in schedule.course_list:
+        courses[c] = (len(schedule.get_node_conflicts(c)), schedule.get_node_conflicts(c))
+    
+    courses_sorted_by_num_of_conflict = dict(sorted(courses.items(), key=lambda x:x[1][0], reverse=True))
+
+    courses_in_conflict_by_slot = []
+    for current_course, current_course_info in courses_sorted_by_num_of_conflict.items() :
+        not_assigned = True
+        start_arr = 0
+        while not_assigned :
+            if start_arr >= len(courses_in_conflict_by_slot):
+                courses_in_conflict_by_slot.append([])
+            if current_course in courses_in_conflict_by_slot[start_arr] :
+                start_arr += 1
+            else :
+                solution[current_course] = start_arr + 1
+                courses_in_conflict_by_slot[start_arr] += current_course_info[1] # adding conflicting class so they can't be assigned to this period
+                not_assigned = False
+    return solution
